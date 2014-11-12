9de9785e2b90ebb6056e065159833c9feddfc3aa
toku <toku.email@gmail.com>
2014-11-12 20:08:01 +0900

    add:led関係の関数

--- RGB_matrix.ino
+++ RGB_matrix.ino
@@ -29,24 +29,22 @@ void tutorial(){
     pattern1[8]=OFF;
 
     while(1){
-        Led::lighting(pattern1,1500);  //pattern1を1500[ms]出力し続ける
+        Led::lightingWhile(pattern1,1500);  //pattern1を1500[ms]出力し続ける
 
-        LedColor pattern2[9];
-        Led::setAll(pattern2,RED);    //すべて同じ色ならsetAll()で設定可能
-        Led::lighting(pattern2,800);     //pattern2を800[ms]出力し続ける
+        Led::setAll(RED);    //すべて同じ色ならsetAll()で設定可能
+        Led::lightingWhile(800);     //800[ms]出力し続ける
 
-        Led::setAll(pattern2,OFF);     //pattern2に上書き
-        Led::lighting(pattern2,2000);     //pattern2を2000[ms]出力し続ける
+        Led::setAll(OFF);     //pattern2に上書き
+        Led::lightingWhile(2000);     //2000[ms]出力し続ける
     }
 }
 
 
 void loop(){
     //700[ms]ごとに発光LEDを遷移
-    LedColor pattern[9];
     for(int i=0;i<9;i++){
-        Led::setAll(pattern,OFF);
-        pattern[i]=WHITE;
-        Led::lighting(pattern,700);
+        Led::setAll(OFF);
+        Led::set(i,WHITE);
+        Led::lightingWhile(700);
     }
 }
--- led_fullcolor_9.h
+++ led_fullcolor_9.h
@@ -13,10 +13,17 @@ enum LedColor {
 
 
 namespace Led{
+    LedColor output[9];
+    int interval;
     void init();
-    void setAll(LedColor data[],LedColor color);
+    void setInterval(int val);
+    void set(byte bit,LedColor color);
+    void setAll(LedColor color);
+    void copy(LedColor in[],LedColor out[]);
+    void lighting();
     void lighting(LedColor data[]);
-    void lighting(LedColor data[],unsigned long wait);
+    void lightingWhile(LedColor data[],unsigned long wait);
+    void lightingWhile(unsigned long wait);
 }
 
  void Led::init(){
@@ -41,11 +48,25 @@ namespace Led{
     digiWrite(11, LOW);
     digiWrite(12, LOW);
     digiWrite(13, LOW);
+
+    setAll(OFF);
+    interval=10;
 }
 
+void Led::setInterval(int val){
+    interval=val;
+}
 
-void Led::setAll(LedColor data[],LedColor color){
-    for(int i=0;i<9;i++) data[i]=color;
+void Led::set(byte bit,LedColor color){
+    output[bit]=color;
+}
+
+void Led::setAll(LedColor color){
+    for(int i=0;i<9;i++) output[i]=color;
+}
+
+void Led::copy(LedColor in[],LedColor out[]){
+    for(int i=0;i<9;i++) out[i]=in[i];
 }
 
 
@@ -63,34 +84,43 @@ void ledWrite(LedColor data[],byte bit){
 }
 
 
-void Led::lighting(LedColor data[]){
-    const int INTERVAL=10;
+void Led::lighting(){
     static unsigned long last_time=0;
     
-    //前回実行(青色発光開始時)からINTERVAL[ms]待つ
-    while( (millis()-last_time) > INTERVAL );
+    //前回実行(青色発光開始時)からinterval[ms]待つ
+    while( (millis()-last_time) > interval );
 
     //red
     colorWrite(OFF);
-    ledWrite(data,0);
+    ledWrite(output,0);
     colorWrite(RED);
-    delay(INTERVAL);
+    delay(interval);
 
     //green
     colorWrite(OFF);
-    ledWrite(data,1);
+    ledWrite(output,1);
     colorWrite(GREEN);
-    delay(INTERVAL);
+    delay(interval);
 
     //BLUE
     colorWrite(OFF);
-    ledWrite(data,2);
+    ledWrite(output,2);
     colorWrite(BLUE);
     last_time = millis();
 }
 
+void Led::lighting(LedColor data[]){
+    copy(data,output);
+    lighting();
+}
 
-void Led::lighting(LedColor data[],unsigned long wait){
+void Led::lightingWhile(LedColor data[],unsigned long wait){
     unsigned long begin = millis();
     while( (millis()-begin)>wait ) lighting(data);
+}
+
+
+void Led::lightingWhile(unsigned long wait){
+    unsigned long begin = millis();
+    while( (millis()-begin)>wait ) lighting();
 }
