bef9d6f8fee66406540feb119c447792e55985a2
toku <toku.email@gmail.com>
2014-11-12 21:11:34 +0900

    fix

--- RGB_matrix.ino
+++ RGB_matrix.ino
@@ -5,19 +5,21 @@
 #define SWITCH_PIN 9
 
 
- void setup(){
+void setup(){
     pinMode( BUZZER_PIN, OUTPUT); 
     pinMode( SWITCH_PIN, INPUT_PULLUP); 
     analogReference(INTERNAL);
     Led::init();
+}
 
+void loop(){
     // tutorial();
+    demo();
 }
 
 
 void tutorial(){
     LedColor pattern1[9];
-
     pattern1[0]=RED;    //[RED,GREEN,BLUE]で色を指定
     pattern1[1]=GREEN;
     pattern1[2]=BLUE;
@@ -27,20 +29,20 @@ void tutorial(){
     pattern1[6]=WHITE;   //[WHITE]を選択可能
     pattern1[7]=OFF;     //光らせない時は[OFF]
     pattern1[8]=OFF;
+    Led::lightingWhile(pattern1,1500);  //pattern1を1500[ms]出力し続ける
 
-    while(1){
-        Led::lightingWhile(pattern1,1500);  //pattern1を1500[ms]出力し続ける
+    Led::setAll(OFF);
+    Led::set(0,BLUE);    //set(led,color):指定ledを指定色に設定
+    Led::set(3,RED);
+    Led::set(5,GREEN);
+    Led::set(8,BLUE);
+    Led::lightingWhile(800);     //設定済みの色で800[ms]出力し続ける
 
-        Led::setAll(RED);    //すべて同じ色ならsetAll()で設定可能
-        Led::lightingWhile(800);     //800[ms]出力し続ける
-
-        Led::setAll(OFF);     //pattern2に上書き
-        Led::lightingWhile(2000);     //2000[ms]出力し続ける
-    }
+    Led::setAll(OFF);     //すべて同じ色ならsetAll()で設定可能
+    Led::lightingWhile(2000);     //設定済みの色で2000[ms]出力し続ける
 }
 
-
-void loop(){
+void demo(){
     //700[ms]ごとに発光LEDを遷移
     for(int i=0;i<9;i++){
         Led::setAll(OFF);
