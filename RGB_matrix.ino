afcabf711a26230f8e19294f1cfa1a17a2465a73
toku <toku.email@gmail.com>
2014-11-19 13:08:42 +0900

    fix add comment

--- RGB_matrix.ino
+++ RGB_matrix.ino
@@ -5,6 +5,7 @@
 #define BUZZER_PIN 8
 #define SWITCH_PIN 9
 #define isPress() (!digitalRead(SWITCH_PIN))
+#define until(x) while(!(x))
 
 PlayMelody melodyA(BUZZER_PIN);
 PlayMelody melodyB(BUZZER_PIN);
@@ -40,15 +41,16 @@ void setup(){
 void loop(){
     
     //音が変わるたびに発光箇所が変わる
-    while( !isPress() ){
+    until(isPress()) {  //スイッチが押されるまで繰り返す
         delay(500);
         melodyA.play();
-        while( melodyA.isPlaying() && !isPress() ){ 
+        while( melodyA.isPlaying() ){   //曲が終わるまで再生を継続
             static unsigned long cnt=0;
             cnt += melodyA.play();
             Led::setAll(OFF);
             Led::set(cnt%9,WHITE);
             Led::lighting();
+            if( isPress() ) break;  //スイッチが押されたら再生停止
         }
         melodyA.stop();
         Led::setAll(OFF);
@@ -59,14 +61,16 @@ void loop(){
 
 
     //音の高さによって色が変わる
-    while( !isPress() ){
+    until(isPress()) {  //スイッチが押されるまで繰り返す
         delay(500);
         melodyB.play();
-        while( melodyB.isPlaying() && !isPress()){ 
+        while( melodyB.isPlaying() ){   //曲が終わるまで再生を継続
             melodyB.play();
+            //現在再生中の音程から色を決定
             LedColor color = int2color( melodyB.getPitch()%7+1 );
             Led::setAll(color);
             Led::lighting();
+            if( isPress() ) break;  //スイッチが押されたら再生停止
         }
         melodyB.stop();
         Led::setAll(OFF);
@@ -77,14 +81,15 @@ void loop(){
 
   
     //とにかくカラフル
-    while( !isPress() ){
+    until(isPress()) {  //スイッチが押されるまで繰り返す
         delay(500);
         melodyC.play();
-        while( melodyC.isPlaying() && !isPress() ){ 
-            if( melodyC.play() )
-                for(int i=0;i<3;i++) 
+        while( melodyC.isPlaying() ){   //曲が終わるまで再生を継続
+            if( melodyC.play() )    //新しい音の再生が始まったら
+                for(int i=0;i<3;i++)    //３回「ランダムに選んだLEDをランダムな色に設定」する
                     Led::set(random(0,8),int2color(random(0,7)));
             Led::lighting();
+            if( isPress() ) break;  //スイッチが押されたら再生停止
         }
         melodyC.stop();
         Led::setAll(OFF);
