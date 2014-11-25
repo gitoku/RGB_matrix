b817280aedb95e80aa0cb33ebb04d02618c59a53
toku <toku.email@gmail.com>
2014-11-25 11:07:09 +0900

    パターンの抽象化(メモリ節約のため)

--- RGB_matrix.ino
+++ RGB_matrix.ino
@@ -5,12 +5,8 @@
 #define BUZZER_PIN 8
 #define SWITCH_PIN 9
 #define isPress() (!digitalRead(SWITCH_PIN))
-#define until(x) while(!(x))
 
-PlayMelody melodyA(BUZZER_PIN);
-PlayMelody melodyB(BUZZER_PIN);
-PlayMelody melodyC(BUZZER_PIN);
-
+PlayMelody melody(BUZZER_PIN);
 
 void setup(){
     pinMode( SWITCH_PIN, INPUT_PULLUP); 
@@ -19,11 +15,9 @@ void setup(){
 
     Led::init();
     Led::setInterval(3);
-
-    melodyA.setMelody( rpg_Melody, rpg_Duration, rpg_Length);
-    melodyB.setMelody( senbonzakura_Melody, senbonzakura_Duration, senbonzakura_Length);
-    melodyC.setMelody( ninjaribanban_Melody, ninjaribanban_Duration, ninjaribanban_Length);
 }
+
+
 //曲リスト
 //[shikinouta] 四季の歌     音符数：83
 //[haurunomame] ハウルのメインテーマ  音符数：69
@@ -41,60 +35,20 @@ void setup(){
 void loop(){
     
     //音が変わるたびに発光箇所が変わる
-    until(isPress()) {  //スイッチが押されるまで繰り返す
-        delay(500);
-        melodyA.play();
-        while( melodyA.isPlaying() ){   //曲が終わるまで再生を継続
-            static unsigned long cnt=0;
-            cnt += melodyA.play();
-            Led::setAll(OFF);
-            Led::set(cnt%9,WHITE);
-            Led::lighting();
-            if( isPress() ) break;  //スイッチが押されたら再生停止
-        }
-        melodyA.stop();
-        Led::setAll(OFF);
-        Led::lighting();
-    }
+    melody.setMelody( rpg_Melody, rpg_Duration, rpg_Length);
+    while( patternFlushPoint() );//スイッチが押されるまで繰り返す
     while( isPress() );  //スイッチが離されるまで待つ
-
 
 
     //音の高さによって色が変わる
-    until(isPress()) {  //スイッチが押されるまで繰り返す
-        delay(500);
-        melodyB.play();
-        while( melodyB.isPlaying() ){   //曲が終わるまで再生を継続
-            melodyB.play();
-            //現在再生中の音程から色を決定
-            LedColor color = int2color( melodyB.getPitch()%7+1 );
-            Led::setAll(color);
-            Led::lighting();
-            if( isPress() ) break;  //スイッチが押されたら再生停止
-        }
-        melodyB.stop();
-        Led::setAll(OFF);
-        Led::lighting();
-    }
+    melody.setMelody( senbonzakura_Melody, senbonzakura_Duration, senbonzakura_Length);
+    while( patternPitch2Color() );  //スイッチが押されるまで繰り返す
     while( isPress() );  //スイッチが離されるまで待つ
 
-
   
-    //とにかくカラフル
-    until(isPress()) {  //スイッチが押されるまで繰り返す
-        delay(500);
-        melodyC.play();
-        while( melodyC.isPlaying() ){   //曲が終わるまで再生を継続
-            if( melodyC.play() )    //新しい音の再生が始まったら
-                for(int i=0;i<3;i++)    //３回「ランダムに選んだLEDをランダムな色に設定」する
-                    Led::set(random(0,8),int2color(random(0,7)));
-            Led::lighting();
-            if( isPress() ) break;  //スイッチが押されたら再生停止
-        }
-        melodyC.stop();
-        Led::setAll(OFF);
-        Led::lighting();
-    }
+    //とにかくカラフル        
+    melody.setMelody( ninjaribanban_Melody, ninjaribanban_Duration, ninjaribanban_Length);
+    while( patternRandom3() );   //スイッチが押されるまで繰り返す
     while( isPress() );  //スイッチが離されるまで待つ
 }
 
@@ -110,4 +64,54 @@ LedColor int2color(int num){
         case 6: return YELLOW;
         case 7: return WHITE;
     }
+}
+
+int patternFlushPoint(){
+    delay(500);
+    melody.play();
+    while( melody.isPlaying() ){   //曲が終わるまで再生を継続
+        static unsigned long cnt=0;
+        cnt += melody.play();
+        Led::setAll(OFF);
+        Led::set(cnt%9,WHITE);
+        Led::lighting();
+        if( isPress() ) return false;   //スイッチが押されたら再生停止
+    }
+    melody.stop();
+    Led::setAll(OFF);
+    Led::lighting();
+    return true;
+}
+
+int patternPitch2Color(){
+    delay(500);
+    melody.play();
+    while( melody.isPlaying() ){   //曲が終わるまで再生を継続
+        melody.play();
+        //現在再生中の音程から色を決定
+        LedColor color = int2color( melody.getPitch()%7+1 );
+        Led::setAll(color);
+        Led::lighting();
+        if( isPress() ) return false;  //スイッチが押されたら再生停止
+    }
+    melody.stop();
+    Led::setAll(OFF);
+    Led::lighting();
+    return true;
+}
+
+int patternRandom3(){
+    delay(500);
+    melody.play();
+    while( melody.isPlaying() ){   //曲が終わるまで再生を継続
+        if( melody.play() )    //新しい音の再生が始まったら
+            for(int i=0;i<3;i++)    //３回「ランダムに選んだLEDをランダムな色に設定」する
+                Led::set(random(0,8),int2color(random(0,7)));
+        Led::lighting();
+        if( isPress() ) return false;  //スイッチが押されたら再生停止
+    }
+    melody.stop();
+    Led::setAll(OFF);
+    Led::lighting();
+    return true;
 }
