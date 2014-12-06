425cb476935991229abbaf99515887936cacc6c7
toku <toku.email@gmail.com>
2014-12-06 10:49:21 +0900

    diet

--- RGB_matrix.ino
+++ RGB_matrix.ino
@@ -23,52 +23,9 @@ void setup(){
 }
 
 
-//曲リスト
-//[shikinouta] 四季の歌     音符数：83
-//[haurunomaintame] ハウルのメインテーマ  音符数：69
-//[takibi] たきび            音符数：49
-//[wakeup] Wake up(120) 
-//[rpg] RPG(sekai no owari)46
-//[oboroduki] おぼろ月夜　　音符数：61
-//[ninnjaribanban] ニンジャりバンバン（114） 
-//[kutsuganaru] くつがなる       音符数：66
-//[mikannnohanasakuoka] みかんの花咲く丘  　音符数：56
-//[senbonzakura] 千本桜(配信用)112 
-//[hakonehachiri] 箱根八里    音符数:163
-
-
 void loop(){
-
-      
     melody.setMelody( haurunomaintame_Melody, haurunomaintame_Duration, haurunomaintame_Length);
     playPattern(patternRandom3,haurunomaintame_Color);
-    
-   
-     
-   // melody.setMelody( shikinouta_Melody, shikinouta_Duration, shikinouta_Length);
-   // playPattern(patternRandom3,allcolor);
-   
-   
-     
-   // melody.setMelody( wakeup_Melody, wakeup_Duration, wakeup_Length);
-   // playPattern(patternRandom3,allcolor);
 
-
-   // melody.setMelody( rpg_Melody, rpg_Duration, rpg_Length);
-   // playPattern(patternFlushPoint,allcolor);
-
-
-   // melody.setMelody( senbonzakura_Melody, senbonzakura_Duration, senbonzakura_Length);
-   // playPattern(patternPitch2Color,allcolor);
-
- 
-     
-   // melody.setMelody( ninjaribanban_Melody, ninjaribanban_Duration, ninjaribanban_Length);
-   // playPattern(patternRandom3,allcolor);
-   
-   
-     
-   // melody.setMelody( takibi_Melody, takibi_Duration, takibi_Length);
-   // playPattern(patternRandom3,allcolor);
 }
 
