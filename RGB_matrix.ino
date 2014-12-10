ca65d20dca72635495ed4418b07f4b90206c8216
toku <toku.email@gmail.com>
2014-12-10 17:48:58 +0900

    demo sketch

--- RGB_matrix.ino
+++ RGB_matrix.ino
@@ -13,22 +13,16 @@ void setup(){
 
 
 void loop(){
-    melody.setMelody( takibi_Melody, takibi_Duration, takibi_Length);
-    playPattern(patternFlushC,allcolor);
-    
-    melody.setMelody( rpg_Melody, rpg_Duration, rpg_Length);
-    playPattern(patternRandom3,allcolor);
-    
+
     melody.setMelody( haurunomaintame_Melody, haurunomaintame_Duration, haurunomaintame_Length);
-    playPattern(patternFlushPoint,haurunomaintame_Color);
 
-    melody.setMelody( merrychristmas_Melody, merrychristmas_Duration, merrychristmas_Length);
+    playPattern(patternFlushPoint,allcolor);
+    playPattern(patternFlushC,allcolor);
     playPattern(patternFlushPointSlow,allcolor);
-
-    melody.setMelody( bell_Melody, bell_Duration, bell_Length);
     playPattern(patternFlushTurn,allcolor);
-    
-    melody.setMelody( letitgo_Melody, letitgo_Duration, letitgo_Length);
+    playPattern(patternPitch2Color,allcolor);
+    playPattern(patternRandom3,allcolor);
     playPattern(patternRandom3Blink,allcolor);
+
 }
 
