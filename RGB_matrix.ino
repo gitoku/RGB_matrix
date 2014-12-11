c375cc0f8b9811a9eed663fd8292ff34e46aab8a
toku <toku.email@gmail.com>
2014-12-11 16:53:05 +0900

    公開版

--- RGB_matrix.ino
+++ RGB_matrix.ino
@@ -10,17 +10,29 @@ void setup(){
     Led::init();
     Led::setInterval(3);
     
-    melody.setMelody( haurunomaintame_Melody, haurunomaintame_Duration, haurunomaintame_Length);
 }
 
 
 void loop(){
+    melody.setMelody( haurunomaintame_Melody, haurunomaintame_Duration, haurunomaintame_Length);
     playPattern(patternFlushPoint,allcolor);
+
+    melody.setMelody( haurunomaintame_Melody, haurunomaintame_Duration, haurunomaintame_Length);
     playPattern(patternFlushC,allcolor);
+
+    melody.setMelody( haurunomaintame_Melody, haurunomaintame_Duration, haurunomaintame_Length);
     playPattern(patternFlushPointSlow,allcolor);
+
+    melody.setMelody( haurunomaintame_Melody, haurunomaintame_Duration, haurunomaintame_Length);
     playPattern(patternFlushTurn,allcolor);
+
+    melody.setMelody( haurunomaintame_Melody, haurunomaintame_Duration, haurunomaintame_Length);
     playPattern(patternPitch2Color,allcolor);
+
+    melody.setMelody( haurunomaintame_Melody, haurunomaintame_Duration, haurunomaintame_Length);
     playPattern(patternRandom3,allcolor);
+
+    melody.setMelody( haurunomaintame_Melody, haurunomaintame_Duration, haurunomaintame_Length);
     playPattern(patternRandom3Blink,allcolor);
 
 }
